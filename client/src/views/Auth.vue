eef6c2c819b0bd787babaa0e9438dd398e21e1fc
João Amorim <JLPAmorim>
2021-11-26 16:05:05 +0000

    Estrutura criada

---
+++ client/src/views/Auth.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div>
+    NAME: <input type="text" v-model="name"> <br/>
+    EMAIL: <input type="text" v-model="email"> <br/>
+    PASSWORD: <input type="text" v-model="password"> <br/>
+    <v-btn @click="signup">signup</v-btn>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  data(){
+    return{
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signup(){
+      console.log(this.name);
+      console.log(this.email);
+      console.log(this.password);
+    }
+  }
+}
+</script>
